279e126a6306d2eb1196b73aa64a05f511aad1cc
pineapplebin <pineapplewing@126.com>
2021-06-12 19:30:35 +0800

    chore: init project

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const Hapi = require('@hapi/hapi')
+
+const server = Hapi.server({
+  port: 3000,
+  host: 'localhost',
+})
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, h) => {
+    return 'Hello world'
+  },
+})
+
+const init = async () => {
+  await server.register({
+    plugin: require('hapi-pino'),
+    options: {
+      prettyPrint: true,
+      logEvents: ['response', 'onPostStart'],
+    },
+  })
+
+  await server.start()
+  console.log('Server running on %s', server.info.uri)
+}
+
+process.on('unhandledRejection', (err) => {
+  console.log(err)
+  process.exit(1)
+})
+
+init()
